d21c71e7b7a7810d81e4b0776b58ccd4843fdaa4
joshz <jzegan@gmail.com>
2011-01-19 17:52:51 +0100

    updated plot library update from revision 1215777 increased axis precision

--- plotting/kplotaxis.cpp
+++ plotting/kplotaxis.cpp
@@ -21,7 +21,7 @@
 #include "kplotaxis.h"
 
 #include <math.h> //for log10(), pow(), modf()
-#include <kdebug.h>
+#include <QDebug>
 
 class KPlotAxis::Private
 {
@@ -111,24 +111,19 @@ int KPlotAxis::tickLabelPrecision() const
 }
 
 void KPlotAxis::setTickMarks( double x0, double length ) {
-	d->m_MajorTickMarks.clear();
-	d->m_MinorTickMarks.clear();
+        d->m_MajorTickMarks.clear();
+        d->m_MinorTickMarks.clear();
 
-	//s is the power-of-ten factor of length:
-	//length = t * s; s = 10^(pwr).  e.g., length=350.0 then t=3.5, s = 100.0; pwr = 2.0
-	double pwr = 0.0;
-	modf( log10( length ), &pwr );
-	double s = pow( 10.0, pwr );
-	double t = length / s;
+        //s is the power-of-ten factor of length:
+        //length = t * s; s = 10^(pwr).  e.g., length=350.0 then t=3.5, s = 100.0; pwr = 2.0
+        double pwr = 0.0;
+        modf( log10( length ), &pwr );
+        double s = pow( 10.0, pwr );
+        double t = length / s;
 
-        qDebug() << "label:" << this->label() << "length:" << length
-                << "\tpwr: " << pwr
-                << "\ts:" << s
-                << "\tt" << t;
-
-	double TickDistance = 0.0; //The distance between major tickmarks
-	int NumMajorTicks = 0; //will be between 3 and 5
-	int NumMinorTicks = 0; //The number of minor ticks between major ticks (will be 4 or 5)
+        double TickDistance = 0.0; //The distance between major tickmarks
+        int NumMajorTicks = 0; //will be between 3 and 5
+        int NumMinorTicks = 0; //The number of minor ticks between major ticks (will be 4 or 5)
 
         //adjust s and t such that t is between 3 and 5:
         if ( t < 3.0 ) {
@@ -155,48 +150,43 @@ void KPlotAxis::setTickMarks( double x0, double length ) {
                 NumMinorTicks = 5;
         }
 
-	//We have determined the number of tickmarks and their separation
-	//Now we determine their positions in the Data space.
+        //We have determined the number of tickmarks and their separation
+        //Now we determine their positions in the Data space.
 
-	//Tick0 is the position of a "virtual" tickmark; the first major tickmark 
-	//position beyond the "minimum" edge of the data range.
-        double Tick0 = x0 - fmod( x0, TickDistance/100 );
-qDebug() << __LINE__ << "x0:" << x0 << "\tTick0:" << Tick0
-        << "\tTickDistance:" << TickDistance << "fmod:" << fmod( x0, TickDistance )
-        << "numMajorTickMarks" << NumMajorTicks;
+        //Tick0 is the position of a "virtual" tickmark; the first major tickmark
+        //position beyond the "minimum" edge of the data range.
+        double Tick0 = x0 - fmod( x0, TickDistance );
+        if ( x0 < 0.0 ) {
+                Tick0 -= TickDistance;
+                NumMajorTicks++;
+        }
 
-	if ( x0 < 0.0 ) {
-		Tick0 -= TickDistance;
-		NumMajorTicks++;
-	}
+        for ( int i=0; i<NumMajorTicks+1; i++ ) {
+                double xmaj = Tick0 + i*TickDistance;
+                if ( xmaj >= x0 && xmaj <= x0 + length ) {
+                        d->m_MajorTickMarks.append( xmaj );
+                }
 
-	for ( int i=0; i<NumMajorTicks+1; i++ ) {
-		double xmaj = Tick0 + i*TickDistance;
-		if ( xmaj >= x0 && xmaj <= x0 + length ) {
-			d->m_MajorTickMarks.append( xmaj );
-		}
-
-		for ( int j=1; j<NumMinorTicks; j++ ) {
-			double xmin = xmaj + TickDistance*j/NumMinorTicks;
-			if ( xmin >= x0 && xmin <= x0 + length ) 
-				d->m_MinorTickMarks.append( xmin );
-		}
-	}
+                for ( int j=1; j<NumMinorTicks; j++ ) {
+                        double xmin = xmaj + TickDistance*j/NumMinorTicks;
+                        if ( xmin >= x0 && xmin <= x0 + length )
+                                d->m_MinorTickMarks.append( xmin );
+                }
+        }
 }
 
 QString KPlotAxis::tickLabel( double val ) const {
-	if ( d->m_labelFmt == 't' ) {
-		while ( val <   0.0 ) val += 24.0;
-		while ( val >= 24.0 ) val -= 24.0;
+        if ( d->m_labelFmt == 't' ) {
+                while ( val <   0.0 ) val += 24.0;
+                while ( val >= 24.0 ) val -= 24.0;
 
-		int h = int(val);
-                int m = 60.*(val - h);
-                kDebug() << QString("val: ") + QString::number(val)
-                         << QString( "%1:%2" ).arg( h, 2, 10, QLatin1Char('0') ).arg( m, 2, 10, QLatin1Char('0') );
-		return QString( "%1:%2" ).arg( h, 2, 10, QLatin1Char('0') ).arg( m, 2, 10, QLatin1Char('0') );
-	}
+                int h = int(val);
+                int m = int( 60.*(val+0.005 - h) );
 
-	return QString( "%1" ).arg( val, d->m_labelFieldWidth, d->m_labelFmt, d->m_labelPrec );
+                return QString( "%1:%2" ).arg( h, 2, 10, QLatin1Char('0') ).arg( m, 2, 10, QLatin1Char('0') );
+        }
+
+        return QString( "%1" ).arg(val, d->m_labelFieldWidth, d->m_labelFmt, d->m_labelPrec );
 }
 
 QList< double > KPlotAxis::majorTickMarks() const
