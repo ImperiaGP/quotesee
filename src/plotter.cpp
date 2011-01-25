ddccfdae7629e85c5866d4b0a360acc2c93f8614
joshz <jzegan@gmail.com>
2011-01-25 01:10:19 +0100

    removed some redundant code, comments, added convenience functions

--- src/plotter.cpp
+++ src/plotter.cpp
@@ -2,6 +2,7 @@
 #include "plotting/kplotwidget.h"
 #include "plotting/kplotobject.h"
 #include "plotting/kplotaxis.h"
+#include "plotting/kplotpoint.h"
 
 #include <QGraphicsLinearLayout>
 #include <kdebug.h>
@@ -29,7 +30,6 @@ Plotter::Plotter(QGraphicsWidget *parent) :
 
     addPlot();
     configPlot();
-//    addPlotPoints();
 
     setLayout(m_layout);
 }
@@ -74,67 +74,58 @@ void Plotter::addPlot()
 
 void Plotter::configPlot()
 {
-//    setContentsMargins(0, 20, 0, 0);
-
-    m_plot->setShowGrid( false ); // no grid please
     m_plot->setBackgroundColor(QColor(255, 255, 255) );
     m_plot->setForegroundColor(QColor(0, 0, 0));
-//    m_plot->setLimits(0.0, 5.0, -1.0, 5.0 );
-    m_plot->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-//    m_plot->setMaximumSize(270, 100);
-//    m_plot->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-    m_plot->setLeftPadding(5);
-    m_plot->setRightPadding(40);
-    m_plot->setBottomPadding(25);
-    m_plot->setTopPadding(5);
+    m_plot->setMinimumSize( 400, 300 );
+    m_plot->setRightPadding(50);
+    m_plot->setAntialiasing( true );
+
+    m_plot->axis(KPlotWidget::BottomAxis)->setTickLabelFormat('t');
     m_plot->axis(KPlotWidget::RightAxis)->setTickLabelsShown(true);
     m_plot->axis(KPlotWidget::BottomAxis)->setTickLabelsShown(true);
-//    m_plot->axis(KPlotWidget::BottomAxis)->setTickMarks(0.0, 0.0);
-    m_plot->axis(KPlotWidget::TopAxis)->setTickLabelsShown(false);
     m_plot->axis(KPlotWidget::LeftAxis)->setTickLabelsShown(false);
-
-    m_plot->axis(KPlotWidget::RightAxis)->setTickMarks(0.0, 0.0);
-    m_plot->axis(KPlotWidget::BottomAxis)->setTickMarks(0.0, 0.0);
     m_plot->setShowGrid(true);
-    m_plot->axis(KPlotWidget::BottomAxis)->setTickLabelFormat('t');
-//    m_plot->axis(KPlotWidget::RightAxis)->setLabel("price");
-    resize( 400, 200);
 }
 
-void Plotter::addPlotPoints()
+double Plotter::toAxisTime(double val)
 {
-//    // Create plot object
-//    KPlotObject * ob = new KPlotObject(Qt::red, KPlotObject::Lines, 2, KPlotObject::Circle);
-//    // Add plot object to chart
-//    m_plot->addPlotObject(ob);
-//    ob->setShowPoints(true);
-//    // Add some random points to the plot object
-//    double s = 0;
-////    srand(time(NULL));
-//    ob->addPoint( QPoint(0, 0) );
-//    for (unsigned int i=0; i<=5; i += 1) {
-//            s = rand()*(4.9)/RAND_MAX;
-//            ob->addPoint( QPoint(i, s), QString::number(s).left(4));
-//            kDebug() << "Adding point [" << i << "," << s << "]" << endl;
-//    }
+    int h = int(val);
+    int m = int(10000 * (val + 0.005 - h)/60);
+    return (double)h + (double)m/100;
 }
+
 
 QList< KPlotObject* > Plotter::plotObjects() const
 {
     return m_plot->plotObjects();
 }
 
-void Plotter::setLimits( double x1, double x2, double y1, double y2 )
+double Plotter::axisStart(double currentTime, int span)
 {
-//    double inc;
+    int h = int(currentTime);
+    int m = int( 100.*(currentTime+0.005 - h) ) - span;
+    if(m < 0)
+    {
+        h -= 1;
+        m = m + 60;
+    }
+    return (double)h + (double)m/100;
+}
 
-//    double *yp1 = &y1, *yp2 = &y2;
-//    double *xp1 = &x1, *xp2 = &x2;
-//    AxisScale::DefineAxis(yp1, yp2, &inc);
+void Plotter::setLimits( double x1, double x2, double xLength, double y1, double y2)
+{
+    double length = axisStart(x2, xLength);
 
-    m_plot->setLimits(x1, x2, y1, y2);
-//    kDebug() << "major tick marks:" << m_plot->axis(KPlotWidget::BottomAxis)->majorTickMarks();
-//    m_plot->setLimits(floor(*xp1), ceil(*xp2), floor(*yp1), ceil(*yp2));
+    qDebug() << QString("%1%2%3%4").arg("x1", -6)
+            .arg("x2", -6)
+            .arg("xLength", -10)
+            .arg("length", -10);
+    qDebug() << QString("%1%2%3%4").arg(x1, -6)
+            .arg(x2, -6)
+            .arg(xLength, -10)
+            .arg(length, -10);
+
+    m_plot->setLimits( toAxisTime(length < x1 ? length : x1), toAxisTime(x2 + 0.01), y1, y2 );
 }
 
 void Plotter::addPlotObject(KPlotObject *object)
