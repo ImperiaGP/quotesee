fc078c709dcb2c4f6108f17211e51e32de17e8e2
joshz <jzegan@gmail.com>
2011-01-25 01:11:09 +0100

    added comments, changed param list for setLimits

--- src/plotter.h
+++ src/plotter.h
@@ -38,7 +38,28 @@ public:
     void setName(QString n) {name = n;}
     void setInfo(QString i) {info = i; }
     void setLabelText(QString text);
-    void setLimits( double x1, double x2, double y1, double y2 );
+
+    /**
+     * Set x and y scale for axis
+     * @param x1 axis origin on left
+     * @param x2 axis end on right, adds one minute to make space for point inserted at time x2'
+     * @param length length of the axis, calculated as x2 - length, expands if x1 < x2 - length
+     * @param y1 lower limit of y axis
+     * @param y2 upper limit of y axis
+     */
+    void setLimits( double x1, double x2, double length, double y1, double y2 );
+
+    /**
+     * Calculate origin of the axis give span and most recently inserted point
+     * @param currentTime most recently inserted point
+     * @param span length of the axis from @currentTime to past
+     * @returns time at which to start the axis
+     */
+    double axisStart(double currentTime, int span);
+
+    /**
+     * Wrapper for KPlotWidget::plotObjects()
+     */
     QList< KPlotObject* > plotObjects() const;
 private:
     QGraphicsLinearLayout *m_layout;
@@ -66,6 +87,8 @@ private:
     QString name;
     QString info;
 
+    double toAxisTime(double val);
+    QString timeToString(double t);
     Plasma::Svg *m_item_background;
 signals:
 
